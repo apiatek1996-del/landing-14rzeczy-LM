08836e115099a5b63842a7da7efa8d01bd0788a1
apiatek1996-del <a.piatek1996@gmail.com>
2026-05-17 20:20:21 +0200

    feat: swap sections order on mobile (Features above PainPoints) using responsive Tailwind order classes

--- app/page.tsx
+++ app/page.tsx
@@ -15,16 +15,22 @@ import Footer from '@/components/sections/Footer';
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen">
-      <TopBanner />
-      <Hero />
-      <TargetAudience />
-      <PainPoints />
-      <Solution />
-      <Features />
-      <About />
-      <Testimonials />
-      <CTA />
-      <Footer />
+      {/* 
+        Wykorzystujemy właściwość Flexbox 'order', aby zarządzać kolejnością sekcji:
+        - Na desktopie (lg:) kolejność jest standardowa i logiczna.
+        - Na telefonach przenosimy całą sekcję Features ("w tej liście między innymi") 
+          nad sekcję PainPoints ("Sprawdź, czy też wpadasz w te pułapki...").
+      */}
+      <div className="order-[1] lg:order-[1]"><TopBanner /></div>
+      <div className="order-[2] lg:order-[2]"><Hero /></div>
+      <div className="order-[3] lg:order-[3]"><TargetAudience /></div>
+      <div className="order-[5] lg:order-[4]"><PainPoints /></div>
+      <div className="order-[6] lg:order-[5]"><Solution /></div>
+      <div className="order-[4] lg:order-[6]"><Features /></div>
+      <div className="order-[7] lg:order-[7]"><About /></div>
+      <div className="order-[8] lg:order-[8]"><Testimonials /></div>
+      <div className="order-[9] lg:order-[9]"><CTA /></div>
+      <div className="order-[10] lg:order-[10]"><Footer /></div>
     </main>
   );
 }
